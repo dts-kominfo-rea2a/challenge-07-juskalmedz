49d859f2865f645675532e5bb5ab0b7c65a248e6
juskalmedz <juskalmedz@gmail.com>
2022-06-12 19:33:19 +0700

    feat: Challenge 07 - Promise | First Attempt

--- promise.js
+++ promise.js
@@ -1,7 +1,18 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    const theaterIXX = await promiseTheaterIXX();
+    const theaterVGC = await promiseTheaterVGC();
+
+    let arrHasil = [...theaterIXX, ...theaterVGC];
+    const resultFilter = arrHasil.filter((obj) => obj.hasil === emosi);
+    return resultFilter.length;
+  } catch (error) {
+    console.log("error");
+  }
+};
 
 module.exports = {
   promiseOutput,
